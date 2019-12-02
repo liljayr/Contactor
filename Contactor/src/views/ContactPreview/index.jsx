d117878693cf0eace0230dc35038db544baaa3b4
liljag18 <liljag18@ru.is>
2019-12-02 12:02:55 +0000

    contact preview started

--- Contactor/src/views/ContactPreview/index.jsx
+++ Contactor/src/views/ContactPreview/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
 
 class ContactPreview extends React.Component {
   render(){
     return (
-      <View style={{ flex: 1 }}>
-      </View>
+      <Text>
+        Contacts!
+      </Text>
     );
   }
 }
