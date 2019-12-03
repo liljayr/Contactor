9bc38984bef995d0c5ccf9747610cb712c495cab
liljag18 <liljag18@ru.is>
2019-12-03 14:25:49 +0000

    preview stuff

--- Contactor/src/views/ContactPreviewView/index.jsx
+++ Contactor/src/views/ContactPreviewView/index.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Text } from 'react-native';
-import ContactPreview from '../../components/ContactPreview';
 import { connect } from 'react-redux';
-//import mapStateToProps from
+import ContactPreview from '../../components/ContactPreview';
+// import mapStateToProps from
 
 class ContactPreviewView extends React.Component {
   constructor(props) {
     super(props);
     const { navigation } = this.props;
     const selectedContact = navigation.getParam('selectedContact', -1);
-    console.log(selectedContact);
     this.state = {
       selectedContact,
     };
-    //console.log(this.state);
+    // console.log(this.state);
   }
 
   render() {
     const { selectedContact } = this.state;
     return (
       <ContactPreview
-      selectedContact={selectedContact}/>
+        selectedContact={selectedContact}
+      />
+    // add Button added here
     );
   }
 }
